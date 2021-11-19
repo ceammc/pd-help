288b76200adc3a6b880941d13e51a16fcec661c8
Serge Poltavski <serge.poltavski@gmail.com>
2021-11-20 01:56:55 +0300

    basic english script update

---
+++ scripts/generate_md_en.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+OUT_DIR="@PROJECT_SOURCE_DIR@/help-en"
+PDDOC_DIR="@PDDOC_SRC_DIR@"
+XLET_DB="@PDDOC_SRC_DIR@/ceammc.db"
+
+mkdir -p "${OUT_DIR}"
+
+for f in "${PDDOC_DIR}/"*.pddoc
+do
+    bn=$(basename $f)
+    md="${bn%.pddoc}.md"
+    echo "generating markdown '$md' ..."
+    pd_doc2md --locale EN \
+      --xlet-db ${XLET_DB} \
+      --example-img "../examples/img/" \
+      --example-pd "../examples/pd/" \
+      "$f" "${OUT_DIR}/${md}"
+    break
+done
+
+exit
+
+pd_lib2md ceammc_lib.xml --output index.md -f
+
+for filename in *.md; do
+    mv "$filename" "../help-en/$filename"
+done
+mv img ../help-en/img
+cp examples ../help-en/examples

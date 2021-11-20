620e64089b075b251b748d045e9af7213ff45388
Serge Poltavski <serge.poltavski@gmail.com>
2021-11-20 05:09:02 +0300

    russian script added

---
+++ scripts/generate_md_ru.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+OUT_DIR="@PROJECT_SOURCE_DIR@/help-ru"
+PDDOC_DIR="@PDDOC_SRC_DIR@"
+XLET_DB="@PDDOC_SRC_DIR@/ceammc.db"
+
+mkdir -p "${OUT_DIR}"
+
+pd_lib2md "@PDDOC_SRC_DIR@/ceammc_lib.xml" \
+  --output "${OUT_DIR}/index.md" \
+  --local RU \
+  --force
+
+pd_cat2md "@PDDOC_SRC_DIR@/ceammc_lib.xml" \
+  --base-dir ${OUT_DIR} \
+  --local RU \
+  --force \
+  --aliases
+
+for f in "${PDDOC_DIR}/"*.pddoc
+do
+    bn=$(basename $f)
+    md="${bn%.pddoc}.md"
+    echo "generating markdown '$md' ..."
+    pd_doc2md --locale RU \
+      --xlet-db ${XLET_DB} \
+      --example-img "../examples/img/" \
+      --example-pd "../examples/pd/" \
+      --no-images \
+      "$f" "${OUT_DIR}/${md}"
+done
